cc95ee7512952d2b1ede9dcf8eb413abe66bd123
lotsman_board <leonidt@lotsman.org>
2023-02-07 14:29:18 +0500

    base lesson3

--- JuniorProject/Assets/scripts/test.cs
+++ JuniorProject/Assets/scripts/test.cs
@@ -8,12 +8,40 @@ public class test : MonoBehaviour
     void Start()
     {
         transform.position = new Vector3(3, 4, 5);
+        
         Debug.Log("A");
+    }
+
+    public IEnumerator testCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        Debug.Log("test");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space)) transform.position = transform.position + new Vector3(0, 0.01f, 0);
         Debug.Log("A");
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += -transform.forward * 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * 0.1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += -transform.right * 0.1f;
+        }
     }
 }
